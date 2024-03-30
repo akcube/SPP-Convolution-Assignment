600a1df1d02306a4d9f9cafaf4ea6140328c07af
akcube <a.kishorekumar@students.iiit.ac.in>
2024-03-30 08:59:18 +0530

    final

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,10 @@
 #include <memory>
 #include <cstdint>
 #include <filesystem>
+#include <string>
 
-namespace solution {
-	std::string compute(const std::string& bitmap_path, const float kernel[3][3], const std::int32_t num_rows, const std::int32_t num_cols){
+namespace solution{
+	std::string compute(const std::string &bitmap_path, const float kernel[3][3], const std::int32_t num_rows, const std::int32_t num_cols){
 		std::string sol_path = std::filesystem::temp_directory_path() / "student_sol.bmp";
 		std::ofstream sol_fs(sol_path, std::ios::binary);
 		std::ifstream bitmap_fs(bitmap_path, std::ios::binary);
@@ -20,9 +21,9 @@ namespace solution {
 		    for(int di = -1; di <= 1; di++)
 		        for(int dj = -1; dj <= 1; dj++) {
 		        	int ni = i + di, nj = j + dj;
-		            if(ni >= 0 and ni < num_rows and nj >= 0 and nj < num_rows) 
+		            if(ni >= 0 and ni < num_rows and nj >= 0 and nj < num_cols) 
 		            	sum += kernel[di+1][dj+1] * img[ni * num_cols + nj];
-		        }
+	        	}
 		    sol_fs.write(reinterpret_cast<char*>(&sum), sizeof(sum));
 		}
 		sol_fs.close();
